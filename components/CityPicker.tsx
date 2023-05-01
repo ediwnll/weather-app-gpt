b07c2546fa10d52c070517008ab8a51506aafbea
Edwin <hewzhongxuan6f@gmail.com>
2023-05-01 23:37:25 +0800

    Added stepzen QL

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -46,7 +46,7 @@ function CityPicker() {
 
   const handleSelectedCity = (option: cityOption) => {
     setSelectedCity(option);
-    router.push(`/location/${option?.value.latitude}/${option?.value.longitude}`)
+    router.push(`/location/${option?.value.name}/${option?.value.latitude}/${option?.value.longitude}`)
   };
 
   return (

f5978c1dc0c0b68cf3d0d6ede3d42ecabea59410
Edwin <hewzhongxuan6f@gmail.com>
2023-05-24 18:27:51 +0800

    miss out a file

--- apollo-client.ts
+++ apollo-client.ts
@@ -1,5 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+export const runtime = "edge";
+
 export const getClient = () => {
   const client = new ApolloClient({
     uri: process.env.API_URL,
